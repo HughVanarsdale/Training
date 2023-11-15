321a9c89b3d1aea04dbcb1f8671f2757b99f8e85
Hugh <hugh.vanarsdale@live.com>
2023-11-15 13:12:05 -0600

    Reading text file. now put into own routine

--- Training/Server/Controllers/ReadTextController.cs
+++ Training/Server/Controllers/ReadTextController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace Training.Server.Controllers
 {
@@ -12,12 +13,43 @@ namespace Training.Server.Controllers
         [HttpGet("{filename}")]
         public string[] Getfile(string filename)
         {
+            List<string>? lines = new List<string>();
+            string? aLine="";
 
-            string[] ret = new string[3];
-            ret[0] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-            ret[1] = "bbbbbbbbbbbbbbbbbbbbb";
-            ret[2] = "ccccccccccccccccccccccccccccccccccccc";
-            return ret;
+            var dir = AppDomain.CurrentDomain.BaseDirectory;
+            // works: StreamReader sr = new StreamReader($@"C:\Users\hp\source\repos\Training\Training\Server\Data\{filename}");
+            
+            // works: StreamReader sr = new StreamReader($@"C:/Users/hp/source/repos/Training/Training/Server/Data/{filename}");
+
+            //C:\Users\hp\source\repos\Training\Training\Server\Data
+
+            dir = dir.Replace("\\", "/");
+            dir = dir.Substring(0, dir.IndexOf("bin"));
+            dir = dir + "/data/" + filename;
+
+            StreamReader sr = new StreamReader(dir);
+
+
+            //"C:\\Users\\hp\\source\\repos\\Training\\Training\\Server\\bin\\Debug\\net6.0\\"	
+
+            do
+            {
+                aLine = sr.ReadLine();
+                
+                if (aLine != null)
+                {
+                    lines.Add(aLine);
+                }
+
+            } while (aLine != null);
+
+            sr.Close(); 
+
+//            string[] ret = new string[3];
+//            ret[0] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+//            ret[1] = "bbbbbbbbbbbbbbbbbbbbb";
+//            ret[2] = "ccccccccccccccccccccccccccccccccccccc";
+            return lines.ToArray();
         }
 
     }
