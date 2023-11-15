015e73f7178193f4eb8b68fe124474cf70c066be
Hugh <hugh.vanarsdale@live.com>
2023-11-15 08:19:13 -0600

    Euler: passed filename to controller. yea!

---
+++ Training/Server/Controllers/ReadTextController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Training.Server.Controllers
+{
+    [Route("api/[controller]")]
+    //[Route("controller")]
+    [ApiController]
+    public class ReadTextController : Controller
+    {
+
+        [HttpGet("{filename}")]
+        public string[] Getfile(string filename)
+        {
+
+            string[] ret = new string[3];
+            ret[0] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            ret[1] = "bbbbbbbbbbbbbbbbbbbbb";
+            ret[2] = "ccccccccccccccccccccccccccccccccccccc";
+            return ret;
+        }
+
+    }
+}
